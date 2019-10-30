2a6689c536ce63b3234417462740eb8801e43896
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2019-10-29 22:21:31 -0400

    two more patterns learned

--- Ch10/10_02/Begin/Bridge/Program.cs
+++ Ch10/10_02/Begin/Bridge/Program.cs
@@ -13,16 +13,22 @@ namespace Bridge.Demonstration
     {
         static void Main(string[] args)
         {
+            //create instances of a text sender and a web sender:
             IMessageSender text = new TextSender();
             IMessageSender web = new WebServiceSender();
 
+            //create a message and put stuff in it:
             Message message = new SystemMessage();
             message.Subject = "A Message";
             message.Body = "Hi there, Please accept this message.";
 
+            //send it via text:
+            //first assign the message to the text instance of a message sender.
             message.MessageSender = text;
             message.Send();
 
+            //send it via web:
+            //first assign the message to the web instance of a message sender.
             message.MessageSender = web;
             message.Send();
 
@@ -88,3 +94,25 @@ namespace Bridge.Demonstration
         }
     }
 }
+
+/*
+Bridge Pattern:
+Decouple an abstraction from its implementation
+so they can be modified independently.
+
+Example will be an app that uses different programs to send messages:
+texting and a web service. The abstraction will send the messages
+but won't know what the method of delivery will be.
+
+In the code, IMessageSender is the abstract bridge (it looks like a plain old
+interface to me, although we call it abstract). Then TextSender and
+WebServiceSender are the two implementations of it.
+
+What makes it a bridge is that different implementation methods
+can be assigned to the same high-level abstract class. (Which actually
+still just sounds like using an interface and various implementing methods?)
+
+
+*/
+
+
--- Ch10/10_02/Begin/Facade/Program.cs
+++ Ch10/10_02/Begin/Facade/Program.cs
@@ -18,12 +18,17 @@ namespace Facade.Demonstration
         static void Main()
         {
             // Facade
+            //create an instance of the top level CollegeLoan
             CollegeLoan collegeLoan = new CollegeLoan();
 
             // Evaluate loan
+            //create the student applying for the load
             Student student = new Student("Hunter Sky");
+
+            //call the facade's method, which calls the subsystem methods:
             bool eligible = collegeLoan.IsEligible(student, 75000);
 
+            //show results:
             Console.WriteLine("\n" + student.Name +
                 " has been " + (eligible ? "Approved" : "Rejected"));
 
@@ -37,10 +42,13 @@ namespace Facade.Demonstration
     /// </summary>
     class CollegeLoan
     {
+        //the facade's subsystems:
+        //create instances of each.
         private Bank _bank = new Bank();
         private Loan _loan = new Loan();
         private Credit _credit = new Credit();
 
+        //high level method takes in basic info.
         public bool IsEligible(Student stud, int amount)
         {
             Console.WriteLine("{0} applies for {1:C} loan\n",
@@ -49,6 +57,18 @@ namespace Facade.Demonstration
             bool eligible = true;
 
             // Verify creditworthyness of applicant
+            //the logic here funnels info to each subsystem
+            //ie, _bank, _loan, _credit.
+            //each subsystem checks its own methods and
+            //returns true or false, as it determines.
+            //this looks familiar, like our refactoring
+            //of the Tasks Api.
+            //(see below for the classes and their methods)
+            //note how "eligible" is a variable in this method
+            //that is dependent on the results of all three other methods.
+            //again, our refactoring creates instances of "subsystems"
+            //then our "facade" relies on the instances of those subsystems
+            //and their methods to build a master result in the facade level.
             if (!_bank.HasSufficientSavings(stud, amount))
             {
                 eligible = false;
@@ -122,3 +142,21 @@ namespace Facade.Demonstration
         }
     }
 }
+
+/*
+
+Facade Pattern:
+Provides a unified interface to a set of interfaces.
+Defines a higher level interface.
+
+IOW, it hides complexities of a system.
+Subsystems implement methods; and the facade level
+knows which subsystem will be needed at a given time.
+
+Our example: High level interface will approve loans,
+handling several lower level processes. Sufficient funds,
+good credit, and no bad loans. 
+
+Facade is the CollegeLoan. Subsystems are Bank, Credit, Loan.
+
+*/
