bdff9337eb06af8703c5547454368a458083fe8d
Gabrielle Sudik <gabrielle.b.sudik@gmail.com>
2019-11-16 07:22:44 -0500

    Complete through end of course

--- Ch10/10_02/Begin/Template/Program.cs
+++ Ch10/10_02/Begin/Template/Program.cs
@@ -13,7 +13,30 @@ namespace TemplatePatternDemo
     {
         static void Main(string[] args)
         {
-            // TODO
+            Console.WriteLine("-- Document Reader - PDF doc. --");
+
+            //create an instance of a DocumentReader.
+            //pdf version.
+            DocumentReader drpdf = new PDFDocument();
+
+            drpdf.OpenDocument(); //will "open" the pdf.
+
+            //create a rtf version:
+            DocumentReader drrtf = new RTFDocument();
+
+            drrtf.OpenDocument(); //will "open" the rtf.
+
+            Console.ReadLine();
+
+            //Prof never says so, but I'm picking up that a lot of these patterns
+            //can be used in place of if/else or case statements.
+            //IE, instead of "if pdf then..." and "else..."
+            //it's like each option goes in its own inheriting class.
+            //it makes encapulation and simple and testing easier.
+
+            //I'm also picking up that we might not need all of these fancy names.
+            //Many of these "patterns" are just inheritance of some sort,
+            //but slightly different as applied to different situations.
         }
     }
 
@@ -27,6 +50,8 @@ namespace TemplatePatternDemo
         }
 
         // steps that will be overidden by subclass
+        //This is the key part of this abstract class.
+        //Here is where some behaviors will change, depending on pdf vs rtf.
         public abstract void InterpretDocumentFormat();
 
         // default step
@@ -47,6 +72,8 @@ namespace TemplatePatternDemo
     //'ConcreteClass'- concrete class
     class PDFDocument : DocumentReader
     {
+        //specific to pdf only:
+        //IRL there would be a lot more, obviously.
         public override void InterpretDocumentFormat()
         {
             Console.WriteLine("Document file is processed with " +
@@ -57,6 +84,7 @@ namespace TemplatePatternDemo
     //'ConcreteClass' - concrete class
     class RTFDocument : DocumentReader
     {
+        //specific to rtf only:
         public override void InterpretDocumentFormat()
         {
             Console.WriteLine("Document file is processed with " +
@@ -64,3 +92,19 @@ namespace TemplatePatternDemo
         }
     }
 }
+
+/*
+
+TEMPLATE PATTERN:
+
+Define the skeleton of an algorithm in an operation,
+deferring some steps to subclasses. The subclasses can redefine steps
+of the algorithm without changing the algorithm's structure.
+
+IE, it allows child classes to change some steps of an algorithm
+without changing the algorithm's structure.
+
+Our example: DocumentReader is the abstract class.
+Two concrete classes: Pdf and Rtf.
+
+ */
